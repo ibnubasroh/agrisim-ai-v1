9b7d0f3fd4a06bd71bb56beacbcc3fd6495b9372
ibnubasroh <43227605+ibnubasroh@users.noreply.github.com>
2026-04-02 00:58:12 +0000

    coba react

--- agrisim-react/vite.config.js
+++ agrisim-react/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/agrisim-react/', // SESUAIKAN DENGAN NAMA REPO GITHUB NANTI
+  base: '/agrisim-ai-v1/', // SESUAIKAN DENGAN NAMA REPO GITHUB NANTI
 })
